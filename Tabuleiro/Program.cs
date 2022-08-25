e8bd277d00914bac116831d9f815662c49a42f76
JOAO PEDRO DOS SANTOS XAVIER <joao.psxavier@senacsp.edu.br>
2022-08-24 22:17:59 -0300

    não sei

--- Tabuleiro/Program.cs
+++ Tabuleiro/Program.cs
@@ -6,7 +6,61 @@ namespace Tabuleiro
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("#####Tabuleiro com Dados#####");
+            int his, player = 1, tabuleiro, posicao, somaD, dados, novaPos;
+            Random r = new Random();
+
+
+            while (player != 20)
+            {
+               
+                dados = r.Next(1, 7);
+                somaD = dados + player;
+                player = somaD;
+                Console.WriteLine("Sua Posição: " + player);
+                Console.WriteLine("Seu Dado: " + dados);
+                Console.ReadLine();
+                Console.Clear();
+
+                if (player == 17)
+                {
+                    Console.WriteLine(@"
+██████   ██████  ██      ███████  ██████  ███    ██  █████  ██████   ██████       ██ ███████ 
+██   ██ ██    ██ ██      ██      ██    ██ ████   ██ ██   ██ ██   ██ ██    ██     ███      ██ 
+██████  ██    ██ ██      ███████ ██    ██ ██ ██  ██ ███████ ██████  ██    ██      ██     ██  
+██   ██ ██    ██ ██           ██ ██    ██ ██  ██ ██ ██   ██ ██   ██ ██    ██      ██    ██   
+██████   ██████  ███████ ███████  ██████  ██   ████ ██   ██ ██   ██  ██████       ██    ██   
+                                                                                             
+                                                                                             ");
+                    Console.WriteLine("Deu ruim, volte para 64... 6ª casa");
+                    player = 6;
+                }
+                if(player == 13)
+                {
+                    Console.WriteLine(@"
+██      ██    ██ ██       █████       ██ ██████  
+██      ██    ██ ██      ██   ██     ███      ██ 
+██      ██    ██ ██      ███████      ██  █████  
+██      ██    ██ ██      ██   ██      ██      ██ 
+███████  ██████  ███████ ██   ██      ██ ██████  
+                                                 
+                                                 
+");
+                    Console.WriteLine("Você saudou a mandioca, avançe duas casas");
+                    player = 15;
+                }
+
+                if (player > 20)
+                {
+                    player = 1;
+                    Console.WriteLine("Você voltou para a : " + player);
+                }
+
+
+
+
+            }
         }
     }
 }
+
