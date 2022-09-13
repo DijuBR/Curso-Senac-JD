dabfbb10e596b01365029f62488ff858d8f3795a
JOAO PEDRO DOS SANTOS XAVIER <joao.psxavier@senacsp.edu.br>
2022-09-12 22:28:24 -0300

    aaaa

--- teste33333/Program.cs
+++ teste33333/Program.cs
@@ -13,10 +13,13 @@ namespace teste33333
         int[] x = new int[50];
         int[] y = new int[50];
 
+        bool derrota;
+
         int frutaX;
         int frutaY;
 
         int cobra = 3;
+        int pontos = 0;
         ConsoleKeyInfo keyInfo = new ConsoleKeyInfo();
         char key = 's';
 
@@ -32,17 +35,17 @@ namespace teste33333
         }
 
         public void WriteBoard()
-        //PRINTAR A TELA (Você pode alterar o tamanho se quiser)
+        //PRINTAR A TELA (Você pode alterar o tamanho se quiser em: Line 9,10)
         {
             Console.Clear();
-            for(int i = 1; i <= (largura + 2); i++)
+            for (int i = 1; i <= (largura + 2); i++)
             {
                 Console.SetCursorPosition(i, 1);
                 Console.WriteLine("_");
             }
-            for (int i = 1; i <= (largura+2); i++)
+            for (int i = 1; i <= (largura + 2); i++)
             {
-                Console.SetCursorPosition(i,(altura+2));
+                Console.SetCursorPosition(i, (altura + 2));
                 Console.WriteLine("-");
             }
             for (int i = 1; i <= (altura + 1); i++)
@@ -52,9 +55,10 @@ namespace teste33333
             }
             for (int i = 1; i <= (altura + 1); i++)
             {
-                Console.SetCursorPosition((largura+2), i);
+                Console.SetCursorPosition((largura + 2), i);
                 Console.WriteLine("|");
             }
+            Console.WriteLine("\nPontuação: " + pontos);
         }
         public void Input()
         //Teclado
@@ -68,16 +72,18 @@ namespace teste33333
         public void Logic()
         {
 
-            if(x[0] <= 1 || x[0] > 52 || y[0] < 2 || y[0] > 22)
+            if (x[0] <= 1 || x[0] > 52 || y[0] < 2 || y[0] > 22)
             {
                 Console.WriteLine("Perdeu!!!!!!!!!");
+                derrota = true;
             }
 
-            for(int i=1; i < cobra; i++)
+            for (int i = 1; i < cobra; i++)
             {
-                if(x[0] == x[i] && y[0]==y[i])
+                if (x[0] == x[i] && y[0] == y[i])
                 {
                     Console.WriteLine("Perdeu 222222!!!!!!!!!");
+                    derrota = true;
                 }
             }
 
@@ -86,14 +92,15 @@ namespace teste33333
                 if (y[0] == frutaY)
                 {
                     cobra++;
+                    pontos += 100;
                     frutaX = rnd.Next(2, (largura - 2));
                     frutaY = rnd.Next(2, (altura - 2));
                 }
             }
-            for(int i = cobra;i>1;i--)
+            for (int i = cobra; i > 1; i--)
             {
-                x[i-1] = x[i - 2];
-                y[i-1] = y[i - 2];
+                x[i - 1] = x[i - 2];
+                y[i - 1] = y[i - 2];
             }
             switch (key)
             {
@@ -110,12 +117,12 @@ namespace teste33333
                     x[0]--;
                     break;
             }
-            for(int i=0;i<=(cobra-1); i++)
+            for (int i = 0; i <= (cobra - 1); i++)
             {
                 WritePoint(x[i], y[i]);
                 WritePoint(frutaX, frutaY);
             }
-            Thread.Sleep(200 - (5 * cobra));
+            Thread.Sleep(200 - (3 * cobra));
         }
 
         public void WritePoint(int x, int y)
@@ -125,9 +132,9 @@ namespace teste33333
         }
         static void Main(string[] args)
         {
-        Snake snake = new Snake();
-            while (true)
-                
+            Snake.start();
+            Snake snake = new Snake();
+            while (!snake.derrota)
             {
                 snake.WriteBoard();
                 snake.Input();
@@ -135,7 +142,27 @@ namespace teste33333
             }
             Console.ReadLine();
         }
-
+        static void start()
+        {
+            Console.WriteLine(@"
+ _________  ___  ___  ________  ________  ________          ________  ________   ________  ___  __    _______      
+|\___   ___\\  \|\  \|\   __  \|\   __  \|\   __  \        |\   ____\|\   ___  \|\   __  \|\  \|\  \ |\  ___ \     
+\|___ \  \_\ \  \\\  \ \  \|\  \ \  \|\ /\ \  \|\  \       \ \  \___|\ \  \\ \  \ \  \|\  \ \  \/  /|\ \   __/|    
+     \ \  \ \ \  \\\  \ \   _  _\ \   __  \ \  \\\  \       \ \_____  \ \  \\ \  \ \   __  \ \   ___  \ \  \_|/__  
+      \ \  \ \ \  \\\  \ \  \\  \\ \  \|\  \ \  \\\  \       \|____|\  \ \  \\ \  \ \  \ \  \ \  \\ \  \ \  \_|\ \ 
+       \ \__\ \ \_______\ \__\\ _\\ \_______\ \_______\        ____\_\  \ \__\\ \__\ \__\ \__\ \__\\ \__\ \_______\
+        \|__|  \|_______|\|__|\|__|\|_______|\|_______|       |\_________\|__| \|__|\|__|\|__|\|__| \|__|\|_______|
+                                                              \|_________|                                         
+                                                                                                                   
+                                                                                                                   
+");
+            Console.WriteLine("                                               PRESSIONE ENTER PARA JOGAR                                           ");
+            Console.WriteLine("W - Cima\n" + "A - Esquerda\n" + "S - Baixo\n" + "D - Direita\n");
+            Console.WriteLine("AVISO: NÃO JOGUE SE TIVER PROBLEMAS COM ATAQUE EPILÉTICO, EPILEPSIA E ESSAS COISAS");
+            Console.WriteLine("A EQUIPE NÃO SE RESPONSABILIZA POR DAUNOS CAUSADOS A QUEM FOR TESTAR O NOSSO JOGO!");
+            Console.ReadLine();
+            
+        }
     }
 }
 
